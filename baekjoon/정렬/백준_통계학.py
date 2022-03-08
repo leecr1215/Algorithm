a4ea491cb05ca9689dfab8310bd8a46572e66ffc
leecr1215 <leecr1215@naver.com>
2022-03-09 00:59:51 +0900

    백준 2108 (python)

---
+++ baekjoon/정렬/백준_통계학.py
@@ -0,0 +1,43 @@
+from collections import Counter
+import sys
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().rstrip())
+    list = [] 
+
+    for i in range(N):
+        num = int(sys.stdin.readline().rstrip())
+        list.append(num)
+
+    list.sort()
+
+    # 산술 평균
+    sum = sum(list)
+    average = int(round(sum / N, 0))
+
+    # 중앙값
+    middle = list[int(N/2)]
+
+    # 최빈값
+    mode_list = Counter(list).most_common()
+    mode_freq = mode_list[0][1]
+    mode = mode_list[0][0]
+    modes = []
+
+    for i in mode_list:
+        if i[1] == mode_freq:
+            modes.append(i[0])
+            
+
+    if len(modes) > 1:
+        modes.sort()
+        mode = modes[1]
+
+    # 범위
+    range = max(list) - min(list)
+
+
+    print(average)
+    print(middle)
+    print(mode)
+    print(range)
