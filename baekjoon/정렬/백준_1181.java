4ebf6df520ff92e713c119e630403b0b3c70ac7d
leecr1215 <leecr1215@naver.com>
2022-03-14 01:28:24 +0900

    백준 1181 java

--- baekjoon/정렬/백준_1181.java
+++ baekjoon/정렬/백준_1181.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.*;
 
 public class 백준_1181 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<String> arr = new ArrayList<>();
+		Set<String> set = new HashSet<>();
 
 		int N = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < N; i++) {
-			arr.add(br.readLine());
+			set.add(br.readLine());
 		}
+
+		ArrayList<String> arr = new ArrayList<String>(set);
 		
 		Collections.sort(arr, new Comparator<String>() {
 			@Override
@@ -24,14 +23,16 @@ public class 백준_1181 {
 				if (s1.length() == s2.length()) {
 					return s1.compareTo(s2);
 				} else {
-					return  s1.length() - s2.length();
+					return s1.length() - s2.length();
 				}
-	
+
 			}
-			});
-		for (int i = 0; i < N; i++) {
+		});
+		
+		for (int i = 0; i < arr.size(); i++) {
 			System.out.println(arr.get(i));
 		}
+
 
 	}
 }
