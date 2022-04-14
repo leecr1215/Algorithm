b97aaaac22a16a7c243db2b6eb8a92a0f69afded
leecr1215 <leecr1215@naver.com>
2022-04-14 16:22:48 +0900

    백준_1085.py

--- baekjoon/기타/test.py
+++ baekjoon/기타/test.py
@@ -1,15 +1,15 @@
 import sys
 if __name__ == '__main__':
-    n,m = map(int, sys.stdin.readline().rstrip().split(" "))
-    arr = []
-    for i in range(n):
-        arr.append(list(map(int, sys.stdin.readline().rstrip().split(" "))))
-    L1, R1 = map(int, sys.stdin.readline().rstrip().split(" "))    
-    L2, R2 = map(int, sys.stdin.readline().rstrip().split(" "))
+    N=int(sys.stdin.readline().rstrip())
+    a = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+    answer = 0
 
-    # 첫번째 공격
-    for i in range(L1-1, R1):
-        for j in range(m):
-            if list[i][j] == 1:
-                list[i][j] = 0
-                continue
+    if N==1:
+        print(1)
+    else:
+        answer = min(a) * N
+        if min(a) != max(a):
+            answer += N-1
+        print(answer)
+       
+
