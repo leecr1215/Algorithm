7317e2cc543b9e15d206edfb53fc58cf421c07a3
leecr1215 <leecr1215@naver.com>
2022-04-28 02:12:38 +0900

    백준_11605.py

--- baekjoon/기타/test.py
+++ baekjoon/기타/test.py
@@ -1,15 +1,6 @@
 import sys
 if __name__ == '__main__':
-    N=int(sys.stdin.readline().rstrip())
-    a = list(map(int, sys.stdin.readline().rstrip().split(" ")))
-    answer = 0
-
-    if N==1:
-        print(1)
-    else:
-        answer = min(a) * N
-        if min(a) != max(a):
-            answer += N-1
-        print(answer)
+    if 3.0%1:
+        print("hi")
        
 
