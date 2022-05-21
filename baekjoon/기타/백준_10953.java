278e6c9c5e13125615b77b1743dfd1d7ed3dfe47
leecr1215 <leecr1215@naver.com>
2022-05-21 23:43:21 +0900

    백준_10953.java

---
+++ baekjoon/기타/백준_10953.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class 백준_10953 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            String[] temp = br.readLine().split(",");
+            System.out.println(Integer.parseInt(temp[0])+Integer.parseInt(temp[1]));
+        }
+
+    }
+}
