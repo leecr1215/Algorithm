5a499dc955b97281d6a0d4fad82768f8a9945402
leecr1215 <leecr1215@naver.com>
2022-06-04 23:48:17 +0900

    백준_11718.py

--- baekjoon/브루트포스/백준_17614.py
+++ baekjoon/브루트포스/백준_17614.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     N = int(sys.stdin.readline().rstrip())
     result = 0
 
-    for i in range(N):
-        if "3" in str(i) or  "6" in str(i) or  "9" in str(i):
-            result += 1
+    for i in range(1,N+1):
+        string = str(i)
+        result += string.count("3")
+        result += string.count("6")
+        result += string.count("9")
     
     print(result)
