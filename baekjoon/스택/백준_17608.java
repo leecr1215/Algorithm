310661aeeaf9cec9b25d04d5bdc9f77e5b34b7ff
leecr1215 <leecr1215@naver.com>
2022-06-13 22:47:16 +0900

    백준_17608.java

---
+++ baekjoon/스택/백준_17608.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class 백준_17608 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < N; i++){
+            stack.push(Integer.parseInt(br.readLine()));
+        }
+
+        int result = 1;
+        int front = stack.pop();
+        int back = 0;
+
+        while(!stack.empty()){
+            back = stack.pop();
+            if(back > front){
+                result += 1;
+            }
+            if(front < back){
+                front = back;
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
