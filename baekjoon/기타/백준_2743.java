376d9ba688f9420db94df693fe00412a2b7a6750
leecr1215 <leecr1215@naver.com>
2022-06-17 23:41:34 +0900

    백준_2743.java

---
+++ baekjoon/기타/백준_2743.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 백준_2743{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String st = sc.nextLine();
+        System.out.println(st.length());
+    }
+}
