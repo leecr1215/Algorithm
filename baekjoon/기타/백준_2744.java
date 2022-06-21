ed8c20a29b92d141ad4d7bac296dab3f2d98ad25
leecr1215 <leecr1215@naver.com>
2022-06-22 00:37:04 +0900

    feat: 백준_2754.py

---
+++ baekjoon/기타/백준_2744.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class 백준_2744 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String st = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < st.length(); i++){
+            char C = st.charAt(i);
+            if(Character.isUpperCase(C)){
+                sb.append(String.valueOf(C).toLowerCase());
+            }else if(Character.isLowerCase(C)){
+                sb.append(String.valueOf(C).toUpperCase());
+            }
+        }
+        System.out.println(sb);
+    }
+}
