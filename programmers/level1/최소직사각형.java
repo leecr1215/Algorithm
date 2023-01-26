2a607861ce23278594ac2f115874a9be9f2c3d0a
leecr1215 <leecr1215@naver.com>
2023-01-26 23:38:22 +0900

    1월 26일 알고리즘 스터디 최소직사각형.java

---
+++ programmers/level1/최소직사각형.java
@@ -0,0 +1,14 @@
+public class 최소직사각형 {
+    public static int solution(int[][] sizes) {
+    	int maxW = 0;
+    	int maxH = 0;
+    	
+    	for(int i = 0; i < sizes.length; i++) {
+    		maxW = Math.max(maxW, Math.max(sizes[i][0], sizes[i][1]));
+    		maxH = Math.max(maxH, Math.min(sizes[i][0], sizes[i][1]));
+
+    	}
+
+        return maxW * maxH;
+    }
+}
