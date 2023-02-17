26986530f95d1bd4cc4b18d7a4d528057029d53a
leecr1215 <leecr1215@naver.com>
2023-02-17 22:40:45 +0900

    2월 17일 프로그래머스 level1 소수 찾기.js

---
+++ programmers/level1/소수 찾기.js
@@ -0,0 +1,21 @@
+function solution(n) {
+  let answer = 0;
+  const arr = new Array(n + 1).fill(true); // 초깃값 설정
+  const end = Math.sqrt(n);
+
+  for (let i = 2; i <= end; ++i) {
+    // 이미 소수가 아닌 인덱스는 건너뛴다.
+    if (!arr[i]) continue;
+
+    // 소수가 아닌 데이터는 false로 입력한다.
+    for (let k = i * 2; k <= n; k += i) {
+      arr[k] = false;
+    }
+  }
+
+  // 소수의 갯수를 구한다.
+  for (let i = 2; i <= n; ++i) {
+    if (arr[i]) answer++;
+  }
+  return answer;
+}
